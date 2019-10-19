1aa63e04b86ca5c4069748e2a23d48ba9d0819e6
ITSW <s-okabe@itsocialwork.com>
2019-10-19 15:20:35 +0900

    Add files via upload

---
+++ workspace/eclipse-artifactory/src/main/java/TestApp.java
@@ -0,0 +1,18 @@
+package com.itsw.jfrog.demo;
+
+/**
+ * MavenでHello World
+ */
+public class TestApp {
+    /**
+     * main処理
+     * 標準出力にHello Worldを表示する。
+     * @param args コマンド引数
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello Maven World!");
+
+    }
+
+}
